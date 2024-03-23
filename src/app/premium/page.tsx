87bc502e2e4de3213ddaa011dff777e41da6d349
Dzserold <f.dzserold@gmail.com>
2024-03-23 23:31:26 +0100

    protect premium page with session

--- src/app/premium/page.tsx
+++ src/app/premium/page.tsx
@@ -1,7 +1,32 @@
-export default function Premium() {
+import { getSession } from "@/actions";
+import Link from "next/link";
+import { redirect } from "next/navigation";
+
+export default async function Premium() {
+  const session = await getSession();
+
+  if (!session.isLoggedIn) {
+    redirect("/");
+  }
+
+  if (!session.isPro) {
+    return (
+      <div className="notPremium">
+        <p>Only premium users can see the content</p>
+        <Link href="/profile">
+          Go to profile page to get premium
+        </Link>
+      </div>
+    );
+  }
   return (
     <div>
       <h1>Wellcome to the Premium page</h1>
+      <ul>
+        <li>Orange</li>
+        <li>Apple</li>
+        <li>Peach</li>
+      </ul>
     </div>
   );
 }
